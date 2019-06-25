c41a2bde9da82b01f88c780bacc59c20dc316002
46353658 <student46353658@gmail.com>
2019-06-25 22:35:12 +0200

    updated profile

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -5,7 +5,11 @@ export class UserService {
   private userInfo = {
     name: "Arshad",
     jobTitle: "[Job Title here]",
-    aboutMe: "[Place your description here... For instance: I am an allround web developer. I am a senior programmer with good knowledge of front-end techniques. I love structure and order and I also stand for quality. I love spending time on fixing little details and optimizing web apps. Also I like working in a team, you'll learn faster and much more. As the saying goes: 'two heads are better than one'.]",
+    aboutMe: `I'm a software engineer (BSc Computing) who has primarily worked in the fintech industry for big names such as First National Bank,
+    Hollard and Sanlam. I've developed in a broad array of tech, from mainframe systems that calculate interest and repayment schedules
+    to Angular on the contact center front end.
+    I've worked in COBOL, Progress 4GL, Java, JavaScript and TypeScript and have filled both development and analyst roles.
+    I'm currently employed by Entelect and am based at Standard Bank doing some devOps and Java on the service layer for the App and Web platforms.`,
     profilePicture: "http://i0.wp.com/cdn.techgyd.com/save-whatsapp-profile-picture-image3.jpg?resize=337%2C337",
     location: "[Your location here]",
     email: "[Your email here]",
