ec98cb0c5e293d84c6d6cab57433f808ebce8282
46353658 <student46353658@gmail.com>
2019-06-28 20:11:17 +0200

    update profile and details

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -5,17 +5,19 @@ export class UserService {
   private userInfo = {
     name: "Arshad",
     jobTitle: "[Job Title here]",
-    aboutMe: `I'm a software engineer (BSc Computing) who has primarily worked in the fintech industry for big names such as First National Bank,
+    aboutMe: `I'm a software engineer (BSc Computing - UNISA), I've primarily worked in the fintech industry for First National Bank,
     Hollard and Sanlam. I've developed in a broad array of tech, from mainframe systems that calculate interest and repayment schedules
-    to Angular on the contact center front end.
-    I've worked in COBOL, Progress 4GL, Java, JavaScript and TypeScript and have filled both development and analyst roles.
-    I'm currently employed by Entelect and am based at Standard Bank doing some devOps and Java on the service layer for the App and Web platforms.`,
-    profilePicture: "http://i0.wp.com/cdn.techgyd.com/save-whatsapp-profile-picture-image3.jpg?resize=337%2C337",
-    location: "[Your location here]",
+    to the contact center front end in Angular and Electron. 
+    
+    I've worked in various programming languages/frameworks and have experience as a System's Analyst.
+    
+    I'm currently employed by Entelect working on devOps and server side Java/Spring for Standard Bank's Web and Mobile platforms.`,
+    profilePicture: "https://images.unsplash.com/photo-1558259167-b4116737909b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    location: "Johannesburg, South Africa",
     email: "[Your email here]",
     devpostAccount: "",
     facebookAccount: "",
-    githubAccount: "[Your GitHub account here]",
+    githubAccount: "https://github.com/46353658",
     googleAccount: "",
     linkedinAccount: "",
     twitterAccount: "",
